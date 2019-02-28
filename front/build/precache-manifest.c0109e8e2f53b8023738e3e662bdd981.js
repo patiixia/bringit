1c76891493fb4038739ae666dc11574ed8790c42
Patricia Martin <patricia_martin_4@hotmail.com>
2019-02-28 13:26:42 +0100

    third commit

---
+++ front/build/precache-manifest.c0109e8e2f53b8023738e3e662bdd981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e12ffeed7a79b1b8ec13",
+    "url": "/static/js/main.e12ffeed.chunk.js"
+  },
+  {
+    "revision": "aea5fc19bf8db96d0d98",
+    "url": "/static/js/2.aea5fc19.chunk.js"
+  },
+  {
+    "revision": "e12ffeed7a79b1b8ec13",
+    "url": "/static/css/main.8cb25a8d.chunk.css"
+  },
+  {
+    "revision": "6ba6ccdb6b1c10394cae97c0bdf8a5f4",
+    "url": "/index.html"
+  }
+];
